d5dc2ecb6ebc165d33e97a93cf94e5062c09ae52
irage <iragebear@mail.ru>
2023-09-07 16:39:26 +0300

    KS-1-2 Two variables

--- src/main/kotlin/lesson_1/lesson_1_task_2.kt
+++ src/main/kotlin/lesson_1/lesson_1_task_2.kt
@@ -4,8 +4,5 @@ fun main() {
     val allOrders: Int = 75
     val thanksForBuy: String = "Спасибо за покупку!"
     println("$allOrders,\n$thanksForBuy")
-    var staffOfCompany = 2000
-    //println(staffOfCompany)
-    println(--staffOfCompany)
 
 }
