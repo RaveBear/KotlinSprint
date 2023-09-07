0d7243829ccf15059b011392453e5f7f47aa75ff
irage <iragebear@mail.ru>
2023-09-07 16:21:39 +0300

    KS-1-2 Staff of company

---
+++ src/main/kotlin/lesson_1/lesson_1_task_2.kt
@@ -0,0 +1,11 @@
+package lesson_1
+
+fun main() {
+    val allOrders: Int = 75
+    val thanksForBuy: String = "Спасибо за покупку!"
+    println("$allOrders,\n$thanksForBuy")
+    var staffOfCompany = 2000
+    //println(staffOfCompany)
+    println(--staffOfCompany)
+
+}
