fbb90b5342bd706415dd692ea17e56d2e4c050ce
irage <iragebear@mail.ru>
2023-09-12 17:57:08 +0300

    KS-2-2 Salary personal and trainee + corrections

--- src/main/kotlin/lesson_1/lesson_1_task_5.kt
+++ src/main/kotlin/lesson_1/lesson_1_task_5.kt
@@ -3,19 +3,13 @@ package lesson_1
 fun main() {
     val allSeconds = 6480
     val hour = allSeconds / 3600
+    val h = String.format("%02d", hour)
+
     val minutes = (allSeconds / 60) % 60
+
     val seconds = allSeconds % 60
+    val s = String.format("%02d", seconds)
+    println("$h:$minutes:$s")
 
-    println("$hour:$minutes:$seconds") /* не знаю как сделать так,
-    чтоб нули оказались перед цифрой "1" и "0". Если только записать их в String */
-
-    val hour1 = allSeconds / 3600
-    hour1.toString()
-    val minutes1 = (allSeconds / 60) % 60
-    minutes1.toString()
-    val seconds1 = allSeconds % 60
-    seconds1.toString()
-
-    println("0$hour1:$minutes1:0$seconds1")
 
 }
