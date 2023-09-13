0310a16c86ac04b742ad04f1b6ed1be835919ae5
irage <iragebear@mail.ru>
2023-09-13 10:20:40 +0300

    KS-2-2 Salary personal and trainee + corrections 2

--- src/main/kotlin/lesson_1/lesson_1_task_5.kt
+++ src/main/kotlin/lesson_1/lesson_1_task_5.kt
@@ -6,10 +6,11 @@ fun main() {
     val h = String.format("%02d", hour)
 
     val minutes = (allSeconds / 60) % 60
+    val m = String.format("%02d", minutes)
 
     val seconds = allSeconds % 60
     val s = String.format("%02d", seconds)
-    println("$h:$minutes:$s")
+    println("$h:$m:$s")
 
 
 }
