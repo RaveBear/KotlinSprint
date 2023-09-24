97234204eae926b39387469061964a8dffc9d19c
RaveBear <iragebear@mail.ru>
2023-09-24 08:27:47 +0300

    KS-2-5 Bank calculator

--- src/main/kotlin/lesson_2/lesson_2_task_5.kt
+++ src/main/kotlin/lesson_2/lesson_2_task_5.kt
@@ -6,7 +6,7 @@ fun main() {
     val clientDeposit = 70_000
     val bidParcent = 16.7 / 100 // in %
 
-    val clientDepositResult = 70_000 * (1 + (0.167 / 365)).pow(7300)
+    val clientDepositResult = clientDeposit * (1 + (bidParcent / 365)).pow(7300)
     println(String.format("%.3f", clientDepositResult))
 
 }
