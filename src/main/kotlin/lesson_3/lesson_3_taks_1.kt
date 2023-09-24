f6ec2b63bf0dfeb66beab1b5dde7718ef24c1a37
RaveBear <iragebear@mail.ru>
2023-09-24 11:24:45 +0300

    KS-3-1 Greeting

---
+++ src/main/kotlin/lesson_3/lesson_3_taks_1.kt
@@ -0,0 +1,11 @@
+package lesson_3
+
+fun main() {
+    val userName = "Иван!"
+    var greetingDayOrNight = "Добрый день"
+    println("$greetingDayOrNight, $userName")
+
+    greetingDayOrNight = "Добрый вечер"
+    println("$greetingDayOrNight, $userName")
+
+}
