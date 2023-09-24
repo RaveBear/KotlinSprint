18dc7a8c98db5c217b1661ed9a4b96d1b01cc911
RaveBear <iragebear@mail.ru>
2023-09-24 11:48:31 +0300

    KS-3-2 Name, Lastname

---
+++ src/main/kotlin/lesson_3/lesson_3_task_2.kt
@@ -0,0 +1,13 @@
+package lesson_3
+
+fun main() {
+    val name = "Татьяна"
+    var lastname = "Андреева"
+    val surname = "Сергеевна"
+    var age = 20
+    println("ФИО: " + lastname + " " + name + " " + surname + ", " + "Возраст: " + age + " лет")
+
+    lastname = "Сидорова"
+    age = 22
+    println("ФИО: " + lastname + " " + name + " " + surname + ", " + "Возраст: " + age + " года")
+}
