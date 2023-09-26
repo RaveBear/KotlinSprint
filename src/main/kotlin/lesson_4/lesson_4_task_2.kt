47a1c21e65b8adf0180eaa6b07f89549241b0760
RaveBear <iragebear@mail.ru>
2023-09-26 22:11:06 +0300

    KS-4-2 Cargo weight and volume

---
+++ src/main/kotlin/lesson_4/lesson_4_task_2.kt
@@ -0,0 +1,23 @@
+package lesson_4
+
+val CARGO_CATEGORY = "Average"
+//val WEIGHT_AVERAGE_CARGO = 35..100
+val MIN_WEIGHT_AVERAGE_CARGO = 35
+val MAX_WEIGHT_AVERAGE_CARGO = 100
+val VOLUME_AVERAGE_CARGO = 100
+
+
+fun main() {
+    val weightCargo1 = 20
+    val volumeCargo1 = 80
+    println("Груз с весом $weightCargo1 кг и объемом $volumeCargo1 л соответствует категории: '$CARGO_CATEGORY':" +
+            "${(weightCargo1 >= MIN_WEIGHT_AVERAGE_CARGO) && (weightCargo1 <= MAX_WEIGHT_AVERAGE_CARGO)
+            and (volumeCargo1 < VOLUME_AVERAGE_CARGO)}")
+
+    val weightCargo2 = 50
+    val volumeCargo2 = 100
+    println("Груз с весом $weightCargo2 кг и объемом $volumeCargo2 л соответствует категории: '$CARGO_CATEGORY':" +
+            "${(weightCargo2 >= MIN_WEIGHT_AVERAGE_CARGO) && (weightCargo2 < MAX_WEIGHT_AVERAGE_CARGO) 
+                    and (volumeCargo2 < VOLUME_AVERAGE_CARGO)}")
+
+}
