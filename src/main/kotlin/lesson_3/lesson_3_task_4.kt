bbd52b90d0b8f7011d3c40702a792f8c17c17168
RaveBear <iragebear@mail.ru>
2023-09-26 21:07:24 +0300

    KS-3-4 Chess + corrections

--- src/main/kotlin/lesson_3/lesson_3_task_4.kt
+++ src/main/kotlin/lesson_3/lesson_3_task_4.kt
@@ -4,11 +4,11 @@ fun main() {
     var moveFrom = "E2"
     var moveIn = "E4"
     var moveNumber = 1
-    println("Ход номер $moveNumber: $moveFrom-$moveIn")
+    println("$moveFrom-$moveIn;$moveNumber")
 
     moveFrom = "D2"
     moveIn = "D3"
     moveNumber ++
-    println("Ход номер $moveNumber: $moveFrom-$moveIn")
+    println("$moveFrom-$moveIn;$moveNumber")
 
 }
