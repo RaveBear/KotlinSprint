162b5dbf7f0787e0d6736e230a4cf026a30243f6
RaveBear <iragebear@mail.ru>
2023-09-27 23:45:21 +0300

    KS-4-2 Cargo weight and volume + corrections

--- src/main/kotlin/lesson_4/lesson_4_task_2.kt
+++ src/main/kotlin/lesson_4/lesson_4_task_2.kt
@@ -1,11 +1,9 @@
 package lesson_4
 
 val CARGO_CATEGORY = "Average"
-//val WEIGHT_AVERAGE_CARGO = 35..100
 val MIN_WEIGHT_AVERAGE_CARGO = 35
 val MAX_WEIGHT_AVERAGE_CARGO = 100
 val VOLUME_AVERAGE_CARGO = 100
-
 
 fun main() {
     val weightCargo1 = 20
@@ -18,6 +16,6 @@ fun main() {
     val volumeCargo2 = 100
     println("Груз с весом $weightCargo2 кг и объемом $volumeCargo2 л соответствует категории: '$CARGO_CATEGORY':" +
             "${(weightCargo2 >= MIN_WEIGHT_AVERAGE_CARGO) && (weightCargo2 < MAX_WEIGHT_AVERAGE_CARGO) 
-                    and (volumeCargo2 < VOLUME_AVERAGE_CARGO)}")
+                    and (volumeCargo2 <= VOLUME_AVERAGE_CARGO)}")
 
 }
