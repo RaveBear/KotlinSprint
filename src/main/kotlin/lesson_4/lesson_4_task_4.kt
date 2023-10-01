1add644fb9250f69ea90db3cf71b14636c178f4f
RaveBear <iragebear@mail.ru>
2023-10-01 16:09:38 +0300

    KS-4-4 Training program + correction

--- src/main/kotlin/lesson_4/lesson_4_task_4.kt
+++ src/main/kotlin/lesson_4/lesson_4_task_4.kt
@@ -1,17 +1,17 @@
 package lesson_4
 
-val ARM_TRAINING = true
-val CRUNCHES_TRAINING = true
-val LEGS_TRAINING = false
-val BACK_TRAINING = false
+const val IS_EVEN_ARM_TRAINING = true
+const val IS_EVEN_CRUNCHES_TRAINING = true
+const val IS_EVEN_LEGS_TRAINING = false
+const val IS_EVEN_BACK_TRAINING = false
 
 fun main() {
     val dayOfTraining = 5
     val trainingToday = dayOfTraining % 2 == 0
 
-    println("Упражнение для рук:    ${trainingToday != ARM_TRAINING}" +
-            "\nУпражнение для ног:    ${trainingToday != LEGS_TRAINING}" +
-            "\nУпражнение для спины:  ${trainingToday != BACK_TRAINING}" +
-            "\nУпражнение для пресса: ${trainingToday != CRUNCHES_TRAINING}")
+    println("Упражнение для рук:    ${trainingToday != IS_EVEN_ARM_TRAINING}" +
+            "\nУпражнение для ног:    ${trainingToday != IS_EVEN_LEGS_TRAINING}" +
+            "\nУпражнение для спины:  ${trainingToday != IS_EVEN_BACK_TRAINING}" +
+            "\nУпражнение для пресса: ${trainingToday != IS_EVEN_CRUNCHES_TRAINING}")
 
 }
