ebb6c3a05a10451439c7dfa542e3299f38f3fd9e
RaveBear <iragebear@mail.ru>
2023-10-03 23:19:14 +0300

    KS-5-1 User authorization + corrections

--- src/main/kotlin/lesson_5/lesson_5_task_1.kt
+++ src/main/kotlin/lesson_5/lesson_5_task_1.kt
@@ -1,13 +1,12 @@
 package lesson_5
 
-const val RIGHT_ANSWER = 7
 
 fun main() {
-
+    val rightAnswer = 2 + 5
     println("Подтвердите, что Вы не робот. Решите задачу: 2 + 5 =")
     val userAnswer = readln().toInt()
 
-    if (userAnswer == RIGHT_ANSWER) {
+    if (userAnswer == rightAnswer) {
         println("Добро пожаловать")
     } else { println("Доступ запрещен") }
 
