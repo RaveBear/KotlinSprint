fc26de948b99d35217ee07cfdc8f84f29a0822f3
RaveBear <iragebear@mail.ru>
2023-10-04 23:06:12 +0300

    KS-4-5 Ship at sea + corrections

--- src/main/kotlin/lesson_4/lesson_4_task_5.kt
+++ src/main/kotlin/lesson_4/lesson_4_task_5.kt
@@ -1,33 +1,30 @@
 package lesson_4
 
-const val HAS_NO_DAMAGE = true
+const val IS_ANY_DAMAGE = false
 const val MIN_CREW_SIZE = 55
 const val MAX_CREW_SIZE = 70
 const val HAS_SUPPLY_BOX = 50
 const val IS_WEATHER_GOOD_OR_NOT = true
 
-// Альтернативно - при незначительных повреждениях может отплыть
-val ALT_CREW_SIZE = 70
-val ALT_HAS_SUPPLY_BOX = 50
-val ALT_IS_WEATHER_GOOD = true
-
 fun main() {
-    println("Повреждения отсутствуют?: ")
+    println("Имеются ли повреждения?: ")
         val damage: Boolean = readln().toBoolean()
 
     println("Состав экипажа, человек: ")
         val crewSize: Int = readln().toInt()
 
     println("Ящики с провизией: шт.")
-    val supplyBox: Int = readln().toInt()
+        val supplyBox: Int = readln().toInt()
 
     println("Погода благоприятная?: ")
-    val weather: Boolean = readln().toBoolean()
+        val weather: Boolean = readln().toBoolean()
 
-    if ((damage == HAS_NO_DAMAGE) && (crewSize in MIN_CREW_SIZE..MAX_CREW_SIZE || crewSize >= ALT_CREW_SIZE)
-        && (supplyBox >= HAS_SUPPLY_BOX || supplyBox >= ALT_HAS_SUPPLY_BOX)
-        && (weather == IS_WEATHER_GOOD_OR_NOT || weather == ALT_IS_WEATHER_GOOD)) {
-            println("Плавание разрешено!")
-        } else {
-            println("Плавание ЗАПРЕЩЕНО!") }
+    if ((damage == IS_ANY_DAMAGE) && (crewSize in MIN_CREW_SIZE..MAX_CREW_SIZE))
+        println("Плавание разрешено")
+
+        else if ((supplyBox >= HAS_SUPPLY_BOX) && (weather == IS_WEATHER_GOOD_OR_NOT))
+        println("Плавание разрешено")
+
+        else
+            println("Плавание запрещено")
     }
