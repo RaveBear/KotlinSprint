9904fa936679307d4f80a0fa429467627c63082c
RaveBear <iragebear@mail.ru>
2023-10-07 18:38:04 +0300

    KS-5-5 Lottery v.2

---
+++ src/main/kotlin/lesson_5/lesson_5_task_5.kt
@@ -0,0 +1,20 @@
+package lesson_5
+
+import kotlin.random.Random
+
+fun main() {
+    val firstWinNum = Random.nextInt(1, 100)
+    println("Введите первое число: ")
+    val userFirstNumber = readln().toInt()
+
+    val secondWinNum = Random.nextInt(1, 100)
+    println("Введите второе число: ")
+    val userSecondNumber = readln().toInt()
+
+    if (userFirstNumber == firstWinNum && userSecondNumber == secondWinNum)
+        println("Вы выиграли главный приз!")
+    else if (userFirstNumber == firstWinNum || userSecondNumber == secondWinNum)
+        println("Вы угадали одно число и получаете утешительный приз!")
+
+    else println("Вы проиграли! Для победы нужны были числа: $firstWinNum и $secondWinNum")
+}
