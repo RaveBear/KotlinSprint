895be0b3f94939b5cb59a5d670ec3adf6ce1f680
RaveBear <iragebear@mail.ru>
2023-10-09 19:40:59 +0300

    KS-6-1 Application authorization

---
+++ src/main/kotlin/lesson_6/lesson_6_task_1.kt
@@ -0,0 +1,29 @@
+package lesson_6
+
+fun main() {
+
+// Регистрация
+    println("Введите имя пользователя:")
+    val regUserName = readln()
+
+    println("Введите пароль:")
+    val regUserPassword = readln()
+    println("Поздравляем! Регистрация прошла успешно.\n")
+
+// Вход в приложение
+    do {
+        println("Имя пользователя:")
+        val entryUserName = readln()
+        println("Пароль:")
+        val entryUserPassword = readln()
+        if (entryUserName == regUserName && entryUserPassword == regUserPassword)
+            println("Авторизация прошла успешно")
+        else
+        break
+
+
+    } while (entryUserName != regUserName || entryUserPassword != regUserPassword)
+        println("Имя пользователя или пароль введены неправильно!\nПопробуйте заново.")
+        return
+
+}
