05d1e5a2b6bd65ce1545a4ed3cd8271bf9f1d3ad
RaveBear <iragebear@mail.ru>
2023-10-15 10:32:40 +0300

    KS-6-2 Game Simple timer + corrections 2

--- src/main/kotlin/lesson_6/lesson_6_task_2.kt
+++ src/main/kotlin/lesson_6/lesson_6_task_2.kt
@@ -2,9 +2,10 @@ package lesson_6
 
 fun main() {
     println("Кол-во секунд, которые нужно засечь: ")
-    var userSeconds = readln().toInt()
+    var userSeconds = readln().toLong()
+    var timerSeconds = userSeconds * 1000
 
     println("Таймер включен")
-        println("Прошло ${userSeconds--} секунд")
-        Thread.sleep(1000)
+    Thread.sleep(timerSeconds)
+    println("Прошло ${userSeconds--} секунд")
 }
