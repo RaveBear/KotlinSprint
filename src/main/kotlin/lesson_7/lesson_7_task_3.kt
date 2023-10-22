e747fc12897aab8bee46117bfea4ba01704539b0
RaveBear <iragebear@mail.ru>
2023-10-22 13:40:10 +0300

    KS-7-3 Even numbers + corrections

--- src/main/kotlin/lesson_7/lesson_7_task_3.kt
+++ src/main/kotlin/lesson_7/lesson_7_task_3.kt
@@ -3,9 +3,7 @@ package lesson_7
 fun main() {
     println("Введите число: ")
     val userNumber = readln().toInt()
-    val range1 = 0..userNumber
-    for (x in range1) {
-        if (x % 2 == 0)
+    for (x in 0..userNumber step 2) {
             println(x)
     }
 
