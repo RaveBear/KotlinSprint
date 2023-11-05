5a683cd2f53e4eaff4c4fffc731b6b56ada77bb1
RaveBear <iragebear@mail.ru>
2023-11-05 21:21:51 +0300

    KS-8-2 Ingredient in recipe

---
+++ src/main/kotlin/lesson_8/lesson_8_task_2.kt
@@ -0,0 +1,19 @@
+package lesson_8
+
+fun main() {
+    val ingrSalad = arrayOf("яйца", "кукуруза", "крабовые палочки", "майонез", "зелень")
+    println("Выберите ингридиент: ")
+    val userIngr = readln()
+    for (x in ingrSalad) {
+        if (userIngr in ingrSalad) {
+            println("Ингридиент $userIngr в рецепте есть")
+            break
+
+        } else {
+            println("Такого ингридиенты в рецепте нет")
+            return
+        }
+
+    }
+
+}
