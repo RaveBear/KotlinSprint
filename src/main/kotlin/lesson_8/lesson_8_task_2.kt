839e003b86af91cda4f7e746f577f55b4bb04cbf
irage <iragebear@mail.ru>
2023-11-06 16:48:24 +0300

    KS-8-4 Ingredient in recipe + corrections

--- src/main/kotlin/lesson_8/lesson_8_task_2.kt
+++ src/main/kotlin/lesson_8/lesson_8_task_2.kt
@@ -4,14 +4,8 @@ fun main() {
     val ingrSalad = arrayOf("яйца", "кукуруза", "крабовые палочки", "майонез", "зелень")
     println("Выберите ингридиент: ")
     val userIngr = readln()
-    for (x in ingrSalad) {
-        if (userIngr in ingrSalad) {
-            println("Ингридиент $userIngr в рецепте есть")
-            break
-
-        } else {
-            println("Такого ингридиенты в рецепте нет")
-            return
-        }
-    }
+    if (userIngr in ingrSalad)
+        println("Ингридиент $userIngr в рецепте есть")
+    else
+        println("Такого ингридиенты в рецепте нет")
 }
